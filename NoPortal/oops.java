1f3b62ea35d95646e347c3139d554ce6a54aab39
Adarsh <iadarshh@gmail.com>
2021-11-15 01:18:55 +0530

    added new loc

--- NoPortal/oops.java
+++ NoPortal/oops.java
@@ -3,25 +3,32 @@ import java.util.*;
 public class oops{
 
     public static void main(String[] Args){
-        Person p1 = new Person();
+
+        Person p1 = new Person(); // Default Constructor 
         p1.name = "Babloo";
         p1.age = 12;
         // System.out.println(p1.name+" is my name & "+p1.age+" is my age");
-
-        Person p2 = new Person();
+        
+        Person p2 = new Person(); // Default Contructor Person() constructs and returns a new object. 
+        // Default contructor doesn't takes any arguments.
         p2.name = "Radha";
         p2.age = 15;
 
-        p1.walk();
         p2.eat();
-        p1.walk(200);
+        p1.walk();        // An example of Polymorphism(Compile time polymorphism -> 
+        p1.walk(200);     //Because it's checking and the decision whether which of the method will get called for after the function call, is happening at the compile time.) 
     }
-
+    
 
     
 }
 
 class Person{     // Every class has two attributes => Property & Methods
+    
+    public Person(){ // Proof that default constructor is being called.
+        System.out.println("creating an object");
+    }
+
     String name;  //Property
     int age;      //Property
 

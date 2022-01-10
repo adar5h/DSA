24ce9deb749a4f3e5582657e74d2c09231b4fc6e
Adarsh <iadarshh@gmail.com>
2022-01-11 01:55:28 +0530

    functions

---
+++ 3_Functions&Arrays/spanOfArray.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class spanOfArray{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int[] arr = new int[n];
+
+    for(int i = 0; i < arr.length; i++){
+        arr[i] = scn.nextInt();
+    }
+    
+    int max = arr[0];
+    int min = arr[0];
+    for(int i = 0; i < arr.length; i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+        if(min>arr[i]){
+            min = arr[i];
+        }
+    }
+
+    System.out.println(max-min);
+ }
+
+}
